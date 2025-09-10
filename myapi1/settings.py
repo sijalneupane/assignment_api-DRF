0b4ca9e0c6a271e084cd270812f4a5ca691efcb9
sijalneupane <sijalneupane5@gmail.com>
2025-09-10 09:58:23 +0545

    Fix: update STATIC_ROOT path in settings.py for production environment

--- myapi1/settings.py
+++ myapi1/settings.py
@@ -140,8 +140,8 @@ STATIC_URL = '/static/'
 
 # This production code might break development mode, so we check whether we're in DEBUG mode
 if not DEBUG:
-    # Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    # Tell Django to copy static assets into a path called `static` (this is specific to Render)
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
     # Enable the WhiteNoise storage backend, which compresses static files to reduce disk use
     # and renames the files with unique names for each version to support long-term caching
