e2fb62a201a12aeaea5290fe2bd2ba9adf45cd1b
sijalneupane <sijalneupane5@gmail.com>
2025-09-14 09:44:34 +0545

    Feat: update build script to include makemigrations and ensure non-interactive migrations and collectstatic

--- build.sh
+++ build.sh
@@ -3,6 +3,8 @@ set -o errexit
 
 pip install -r requirements.txt
 
-python manage.py collectstatic --no-input
+python manage.py makemigrations --noinput
 
-python manage.py migrate
+python manage.py migrate --noinput
+
+python manage.py collectstatic --noinput
