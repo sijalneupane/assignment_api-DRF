7d8edf29fc6efbe0cdf65877257352315e115486
sijalneupane <sijalneupane5@gmail.com>
2025-09-17 16:36:53 +0545

    Refactor: comment out unused signal handling code in signals.py and ready method in apps.py

--- notices/apps.py
+++ notices/apps.py
@@ -4,5 +4,5 @@ from django.apps import AppConfig
 class NoticesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'notices'
-    def ready(self):
-        import notices.signals
+    # def ready(self):
+    #     import notices.signals
--- notices/signals.py
+++ notices/signals.py
@@ -1,36 +1,36 @@
-from django.dispatch import receiver
-from django.db.models.signals import post_save
-from .models import Notices
-from assignments.models import CustomDevice
-from core.models import CustomUser  # adjust if needed
-from fcm_django.admin import Notification,Message
+# from django.dispatch import receiver
+# from django.db.models.signals import post_save
+# from .models import Notices
+# from assignments.models import CustomDevice
+# from core.models import CustomUser  # adjust if needed
+# from fcm_django.admin import Notification,Message
 
-@receiver(post_save, sender=Notices)
-def notify_notice_saved(sender, instance, created, **kwargs):
-    if created:
-        if instance.target_audience == ["ALL"]:
-            devices = CustomDevice.objects.all()
-        else:
-            # First, get user IDs whose faculty matches the target audience
-            user_ids = CustomUser.objects.filter(faculty__in=instance.target_audience).values_list('id', flat=True)
+# @receiver(post_save, sender=Notices)
+# def notify_notice_saved(sender, instance, created, **kwargs):
+#     if created:
+#         if instance.target_audience == ["ALL"]:
+#             devices = CustomDevice.objects.all()
+#         else:
+#             # First, get user IDs whose faculty matches the target audience
+#             user_ids = CustomUser.objects.filter(faculty__in=instance.target_audience).values_list('id', flat=True)
 
-            # Then, get devices for those users
-            devices = CustomDevice.objects.filter(user_id__in=user_ids)
+#             # Then, get devices for those users
+#             devices = CustomDevice.objects.filter(user_id__in=user_ids)
 
-        # for device in devices:
-        #     # send_notification(device, instance.title, instance.content)
-        #     pass
-        devices.send_message(
-            Message(
-            notification=Notification(
-                title="📢 New Notice",
-                image=instance.notice_image.file_url if instance.notice_image else None,
-                body=instance.title
-            ),
-            data={
-                "notice_id": str(instance.notice_id),
-                "target_audience": str(instance.target_audience),
-                "route": "/getNotice"
-            }
-            )
-        )
+#         # for device in devices:
+#         #     # send_notification(device, instance.title, instance.content)
+#         #     pass
+#         devices.send_message(
+#             Message(
+#             notification=Notification(
+#                 title="📢 New Notice",
+#                 image=instance.notice_image.file_url if instance.notice_image else None,
+#                 body=instance.title
+#             ),
+#             data={
+#                 "notice_id": str(instance.notice_id),
+#                 "target_audience": str(instance.target_audience),
+#                 "route": "/getNotice"
+#             }
+#             )
+#         )
